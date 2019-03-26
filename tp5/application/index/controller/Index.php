0bde2c47c507271288dac173d9757f1e10001447
WangXH <wxhkekeke@gmail.com>
2019-03-26 21:25:48 +0800

    test of database

--- tp5/application/index/controller/Index.php
+++ tp5/application/index/controller/Index.php
@@ -2,73 +2,14 @@
 
 namespace app\index\controller;
 
-use think\Controller;
 use think\Db;
-use think\Request;
 
-class Index extends Controller
+class Index
 {
-    use \traits\controller\Jump;
     public function index()
     {
-        $data = Db::name('data')->find();
-        $this->assign('result', $data);
-        return $this->fetch();
-    }
-
-    public function hello(Request $request, $name = 'World')
-    {
-        echo '路由信息：';
-        dump($request->routeInfo());
-        echo '调度信息：';
-        dump($request->dispatch());
-
-        return 'Hello,' . $name . '！';
-    }
-
-    public function request($name = 'World')
-    {
-        $request = Request::instance();
-        // 获取当前URL地址 不含域名
-        echo 'url: ' . $request->url() . '<br/>';
-        return 'Hello,' . $name . '！';
-    }
-
-    public function t32(Request $request, $name = 'World')
-    {
-        // 获取当前域名
-        echo 'domain: ' . $request->domain() . '<br/>';
-        // 获取当前入口文件
-        echo 'file: ' . $request->baseFile() . '<br/>';
-        // 获取当前URL地址 不含域名
-        echo 'url: ' . $request->url() . '<br/>';
-        // 获取包含域名的完整URL地址
-        echo 'url with domain: ' . $request->url(true) . '<br/>';
-        // 获取当前URL地址 不含QUERY_STRING
-        echo 'url without query: ' . $request->baseUrl() . '<br/>';
-        // 获取URL访问的ROOT地址
-        echo 'root:' . $request->root() . '<br/>';
-        // 获取URL访问的ROOT地址
-        echo 'root with domain: ' . $request->root(true) . '<br/>';
-        // 获取URL地址中的PATH_INFO信息
-        echo 'pathinfo: ' . $request->pathinfo() . '<br/>';
-        // 获取URL地址中的PATH_INFO信息 不含后缀
-        echo 'pathinfo: ' . $request->path() . '<br/>';
-        // 获取URL地址中的后缀信息
-        echo 'ext: ' . $request->ext() . '<br/>';
-
-        return 'Hello,' . $name . '！';
-    }
-
-    public function t331()
-    {
-        $data = ['name' => 'thinkphp', 'status' => '1'];
-        return json($data ,200);
-    }
-
-    public function t332()
-    {
-        $data = ['name' => 'thinkphp', 'status' => '1'];
-        return json($data, 201, ['Cache-control' => 'no-cache,must-revalidate']);
+        $result = Db::execute('delete from think_data where id = 5 ');
+        dump($result);
+        return $result;
     }
 }
---
+++ tp5/application/index/model/User.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wxhke
+ * Date: 2019/3/26
+ * Time: 17:40
+ */
+namespace app\index\model;
+
+use think\Model;
+
+class User extends Model
+{
+}
