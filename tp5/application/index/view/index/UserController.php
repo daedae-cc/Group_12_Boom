f8da21d4ef62dbc40a9e1c6dc4569cb55cfa59ee
WangXH <xihao.wang@ucdconnect.ie>
2019-03-28 22:13:57 +0800

    3/28

---
+++ tp5/application/index/view/index/UserController.php
@@ -0,0 +1,87 @@
+<?php
+namespace app\index\controller;     // 该文件的位于application\index\controller文件夹
+
+use think\Controller;               // 用于与V层进行数据传递
+use think\Request;                  // 引用Request
+
+use app\common\model\Teacher;       // 教师模型
+
+/**
+ * 教师管理，继承think\Controller后，就可以利用V层对数据进行打包了。
+ */
+class TeacherController extends Controller
+{
+    public function index()
+    {
+        $Teacher = new Teacher; 
+        $teachers = $Teacher->select();
+
+        // 向V层传数据
+        $this->assign('teachers', $teachers);
+
+        // 取回打包后的数据
+        $htmls = $this->fetch();
+
+        // 将数据返回给用户
+        return $htmls;
+    }
+
+    /**
+     * 插入新数据
+     * @return   html                   
+     * @author 梦云智 http://www.mengyunzhi.com
+     * @DateTime 2016-11-07T12:31:24+0800
+     */
+    public function insert()
+    {
+        // 接收传入数据
+        $postData = Request::instance()->post();    
+
+        // 实例化Teacher空对象
+        $Teacher = new Teacher();
+
+        // 为对象赋值
+        $Teacher->name = $postData['name'];
+        $Teacher->username = $postData['username'];
+        $Teacher->sex = $postData['sex'];
+        $Teacher->email = $postData['email'];
+
+        // 新增对象至数据表
+        $result = $Teacher->validate(true)->save();
+
+        // 反馈结果
+        if (false === $result) {
+            return '新增失败:' . $Teacher->getError();
+        } else {
+            return  '新增成功。新增ID为:' . $Teacher->id;
+        }
+    }
+
+    /**
+     * 新增数据交互
+     * @author 梦云智 http://www.mengyunzhi.com
+     * @DateTime 2016-11-07T12:41:23+0800
+     */
+    public function add()
+    {
+        $htmls = $this->fetch();
+        return $htmls;
+    }
+
+    public function delete()
+    {
+        // 获取要删除的对象
+        $Teacher = Teacher::get(14);
+
+        // 要删除的对象存在
+        if (!is_null($Teacher)) {
+            // 删除对象
+            if ($Teacher->delete()) {
+                return '删除成功';
+            }
+        }
+
+        return '删除失败';
+    }
+}
+
